43fa32c4a6cc48e1870ca7221d65d0a9d89c8984
leovdn <leo_vdn@hotmail.com>
2020-10-08 18:08:49 -0300

    Amazon SES email service configuration

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'leonardo@leovdn.dev',
+      name: 'Leonardo Developer',
+    },
+  },
+} as IMailConfig;
