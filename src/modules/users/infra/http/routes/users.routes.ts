e5138489bf71b56f7f6d1084fda29c693aaa39af
leovdn <leo_vdn@hotmail.com>
2020-10-09 15:18:31 -0300

    Amazon S3 File uploads

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -9,7 +9,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 import ensureAuthenticaded from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
